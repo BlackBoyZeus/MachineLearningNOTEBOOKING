84fde0fb10deabda227f137569cd6aa1848b0c02
Pegasi Assocation <128257630+ThePegasi@users.noreply.github.com>
2023-06-23 13:30:10 -0700

    Update xyzcoordinate.py

--- xyzcoordinate.py
+++ xyzcoordinate.py
@@ -1,7 +1,33 @@
 import numpy as np
 import cv2
 
-def find_object_coordinates(image_points, camera_matrix, rotation_vector, translation_vector):
+def convert_image_points_to_homogeneous_coordinates(image_points):
+    """
+    Convert image points to homogeneous coordinates.
+
+    Args:
+        image_points: The image points in 2D.
+
+    Returns:
+        The image points in homogeneous coordinates.
+    """
+
+    return np.concatenate((image_points, np.ones((len(image_points), 1))), axis=1)
+
+def compute_object_coordinates_in_real_world_coordinates(image_points, camera_matrix, rotation_vector, translation_vector):
+    """
+    Compute the object coordinates in real-world coordinates.
+
+    Args:
+        image_points: The image points in homogeneous coordinates.
+        camera_matrix: The camera matrix.
+        rotation_vector: The rotation vector.
+        translation_vector: The translation vector.
+
+    Returns:
+        The object coordinates in real-world coordinates.
+    """
+
     # Convert rotation vector to rotation matrix
     rotation_matrix, _ = cv2.Rodrigues(rotation_vector)
 
@@ -9,14 +35,11 @@ def find_object_coordinates(image_points, camera_matrix, rotation_vector, transl
     inv_rotation_matrix = np.linalg.inv(rotation_matrix)
     inv_translation_vector = np.dot(-inv_rotation_matrix, translation_vector)
 
-    # Convert image points to homogeneous coordinates
-    image_points_homogeneous = np.concatenate((image_points, np.ones((len(image_points), 1))), axis=1)
-
     # Calculate camera matrix inverse
     camera_matrix_inv = np.linalg.inv(camera_matrix)
 
     # Compute object points in camera coordinates
-    object_points_camera_coords = np.dot(camera_matrix_inv, image_points_homogeneous.T).T
+    object_points_camera_coords = np.dot(camera_matrix_inv, image_points.T).T
 
     # Compute object points in world coordinates
     object_points_world_coords = np.dot(inv_rotation_matrix, object_points_camera_coords.T).T + inv_translation_vector
@@ -39,14 +62,19 @@ def main():
     # Define image points of the object
     image_points = np.array([[200, 150], [250, 150], [250, 200], [200, 200]], dtype=np.float32)
 
-    # Find object coordinates in real-world coordinates
-    object_coordinates = find_object_coordinates(image_points, camera_matrix, rotation_vector, translation_vector)
+    # Convert image points to homogeneous coordinates
+    image_points_homogeneous = convert_image_points_to_homogeneous_coordinates(image_points)
+
+    # Compute object coordinates in real-world coordinates
+    object_coordinates_world_coords = compute_object_coordinates_in_real_world_coordinates(image_points_homogeneous, camera_matrix, rotation_vector, translation_vector)
 
     # Print the object coordinates
-    for i, point in enumerate(object_coordinates):
+    for i, point in enumerate(object_coordinates_world_coords):
         print(f"Point {i+1}: x={point[0]:.2f}m, y={point[1]:.2f}m, z={point[2]:.2f}m")
 
 if __name__ == "__main__":
+    main()
+
     main()
 #In this code, we define a function find_object_coordinates that takes image points, camera matrix, rotation vector, and translation vector as input. It calculates the x, y, z coordinates of the object in real-world coordinates using geometric transformations.
 
