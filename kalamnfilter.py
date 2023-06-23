f4620743e399fef721d49c1a066c5db8c452c5d4
Pegasi Assocation <128257630+ThePegasi@users.noreply.github.com>
2023-06-23 13:26:45 -0700

    Create kalamnfilter.py

---
+++ kalamnfilter.py
@@ -0,0 +1,106 @@
+import cv2
+import numpy as np
+from scipy.linalg import block_diag
+
+class KalmanFilter:
+    def __init__(self, state_dim, measurement_dim):
+        # Initialize state dimensions
+        self.state_dim = state_dim
+        self.measurement_dim = measurement_dim
+
+        # Initialize Kalman filter matrices
+        self.transition_matrix = np.eye(state_dim)
+        self.observation_matrix = np.eye(measurement_dim, state_dim)
+        self.process_noise_cov = np.eye(state_dim)
+        self.measurement_noise_cov = np.eye(measurement_dim)
+        self.error_cov_post = np.eye(state_dim)
+
+        # Initialize state and covariance
+        self.state_post = np.zeros(state_dim)
+        self.error_cov_post = np.eye(state_dim)
+
+    def predict(self):
+        # Predict the state and covariance
+        self.state_pre = np.dot(self.transition_matrix, self.state_post)
+        self.error_cov_pre = np.dot(np.dot(self.transition_matrix, self.error_cov_post), self.transition_matrix.T) + self.process_noise_cov
+
+    def update(self, measurement):
+        # Calculate Kalman gain
+        kalman_gain = np.dot(np.dot(self.error_cov_pre, self.observation_matrix.T), np.linalg.inv(np.dot(np.dot(self.observation_matrix, self.error_cov_pre), self.observation_matrix.T) + self.measurement_noise_cov))
+
+        # Update the state and covariance
+        self.state_post = self.state_pre + np.dot(kalman_gain, (measurement - np.dot(self.observation_matrix, self.state_pre)))
+        self.error_cov_post = np.dot((np.eye(self.state_dim) - np.dot(kalman_gain, self.observation_matrix)), self.error_cov_pre)
+
+class VisualNavigation:
+    def __init__(self):
+        # Initialize the camera
+        self.camera = cv2.VideoCapture(0)
+
+        # Initialize the Kalman filter
+        self.kalman_filter = KalmanFilter(state_dim=4, measurement_dim=2)
+
+    def run(self):
+        while True:
+            # Read the current frame from the camera
+            ret, frame = self.camera.read()
+
+            if not ret:
+                break
+
+            # Perform object detection and get object position
+            object_position = self.detect_object(frame)
+
+            if object_position is not None:
+                # Extract x and y coordinates from object position
+                x, y = object_position
+
+                                # Prepare the measurement vector
+                measurement = np.array([x, y])
+
+                # Predict the state using the Kalman filter
+                self.kalman_filter.predict()
+
+                # Update the state based on the measurement
+                self.kalman_filter.update(measurement)
+
+                # Get the estimated object position from the Kalman filter
+                estimated_position = self.kalman_filter.state_post[:2]
+
+                # Display the current frame with object position and estimated position
+                self.display_frame(frame, object_position, estimated_position)
+
+            # Exit if 'q' is pressed
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        # Release the camera and close all windows
+        self.camera.release()
+        cv2.destroyAllWindows()
+
+    def detect_object(self, frame):
+        # Perform object detection and return object position (x, y)
+        # You can use any object detection algorithm or library here
+        # For the sake of simplicity, let's assume a fixed object position for demonstration
+        object_position = (100, 100)
+        return object_position
+
+    def display_frame(self, frame, object_position, estimated_position):
+        # Display the frame with object position and estimated position
+        cv2.circle(frame, object_position, 5, (0, 255, 0), -1)
+        cv2.circle(frame, tuple(map(int, estimated_position)), 5, (255, 0, 0), -1)
+        cv2.imshow("Visual Navigation", frame)
+
+if __name__ == "__main__":
+    navigation = VisualNavigation()
+    navigation.run()
+
+
+#In this code, we have two main classes: KalmanFilter and VisualNavigation. The KalmanFilter class represents the Kalman filter algorithm and provides methods for initialization, prediction, and update steps. The VisualNavigation class handles the camera input, object detection, and the visual navigation process.
+
+#The run method of the VisualNavigation class is the main loop that reads frames from the camera, performs object detection, and estimates the object position using the Kalman filter. It then displays the current frame with the object position and the estimated position.
+
+#The detect_object method in the VisualNavigation class is a placeholder for the object detection process. You can replace it with your preferred object detection algorithm or library.
+
+
+
